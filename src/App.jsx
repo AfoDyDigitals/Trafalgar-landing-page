1a1a5daa8b18684b23b00000aaabc53061270c62
AfoDy <afodydigitals@gmail.com>
2023-11-22 19:42:29 -0800

    complete Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Hero from './Hero'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   )
 }
